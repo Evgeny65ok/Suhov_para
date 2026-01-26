fae3bbe50bbaca08f88e95f1905bd4a17df5c512
EvgeOk1 <evg65b@gmail.com>
2026-01-26 10:48:23 +0000

    Работа

--- HW.cpp
+++ HW.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    std::cout << "HELLo world!\n"; //вывод на экран
+    std::cerr << "ERROR!!!" << std::endl; //вывод о ошибках
+    std::cout << "крутой \n";
+    int a;
+    std::cin >> a; //ввод с клавиатуры
+    return a;
 }
+// ./HW Что бы программа заработала
+// g++ -o HW HW.cpp что бы скомпилировалось ./ 
+// echo $? - что бы вернуть перменную или ноль
+// \n или << std::endl; что бы перети на новую строку
